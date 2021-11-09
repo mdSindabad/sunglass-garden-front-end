eb419176ae29ae5708042d6e0af3c531dc1a468a
Md. Sindabad <pavel.sindabad@gmail.com>
2021-11-10 00:33:25 +0600

    added header section and maretial ui icon installed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header />
       <h1>Sunglass Garden</h1>
     </div>
   );
